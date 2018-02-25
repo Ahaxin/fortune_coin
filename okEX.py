92a387ff1c0473dea17447dda36bbd9d55321adf
Ahaxin <justin.xin.chang@gmail.com>
2018-02-25 23:55:55 +0100

    Add support for Binance, OKEX (undergoinf)

---
+++ okEX.py
@@ -0,0 +1,46 @@
+# Extract Trade Info from Binance
+import requests
+import json
+
+
+def extract(TP):
+    if TP == "eth_btc":
+        url = "https://www.binance.com/api/v1/depth?symbol=ETHBTC"
+        print("Trading Pair is ETH<->BTC")
+    elif TP == "xrp_btc":
+        url = "https://www.binance.com/api/v1/depth?symbol=XRPBTC"
+        print("Trading Pair is XRP<->BTC")
+    elif TP == "xml_btc":
+        url = "https://www.okex.com/v2/markets/xlm_btc/depth"
+        print("Trading Pair is XML<->BTC")
+    elif TP == "ltc_btc":
+        url = "https://www.binance.com/api/v1/depth?symbol=LTCBTC"
+        print("Trading Pair is LTC<->BTC")
+    else:
+        print("Currently not support this trading pairs %s", TP)
+        return [], []
+    response = requests.get(url)
+    content = response.content.decode()
+    # Load BIDS List
+    bids_list = json.loads(content)['data']
+    bid_price_list = []
+    print("Trading Pair is XRP<->BTC")
+    print(bids_list)
+    for bid in bids_list:
+        price = bid[0]
+        amount = bid[0]
+        print(price)
+        print(amount)
+        bid_price_list.append(price)
+    # Load ASKS List
+    asks_list = json.loads(content)['asks']
+    ask_price_list = []
+    for ask in asks_list:
+        price = ask['price']
+        amount = ask['totalSize']
+        ask_price_list.append(price)
+
+    print('Bids <=============> Asks')
+    for n, g in zip(bid_price_list, ask_price_list):
+        print(n + '\t\t\t' + g)
+    return bids_list, asks_list
