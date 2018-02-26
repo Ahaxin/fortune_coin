a9f438350d316f91f6f68db507c9bfc54215d577
Ahaxin <justin.xin.chang@gmail.com>
2018-02-26 15:54:55 +0100

    Fix few bugs, finish the extract offer function for OKEX

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import okEX
 trading_pairs=['xml_eurt','xml_cny','xml_xrp','xml_ltc','xml_eth','xml_btc','xrp_btc']
 
 for TP in trading_pairs:
-#    print("========== StellarTerm =========")
-#    bids_list, asks_list=stellarTerm.extract(TP)
-#    print(bids_list)
-#    print(asks_list)
+    print("========== StellarTerm =========")
+    bids_list, asks_list=stellarTerm.extract(TP)
+    print(asks_list)
+    print(bids_list)
 #    print("==========   Binance   =========")
 #   bids_list_b, asks_list_b=binance.extract(TP)
 #   print(bids_list_b)
 #    print(asks_list_b)
     print("==========   OKEX   =========")
-    bids_list_b, asks_list_b=okEX.extract('xml_btc')
-#    print(bids_list_b)
-#    print(asks_list_b)
+    bids_list_b, asks_list_b=okEX.extract(TP)
+    print(asks_list_b)
+    print(bids_list_b)
 
--- stellarTerm.py
+++ stellarTerm.py
@@ -14,7 +14,7 @@ def extract(TP):
         print("Trading Pair is XML<->XRP")
     elif TP == "xml_ltc":
         url = "https://horizon.stellar.org/order_book?selling_asset_type=credit_alphanum4&selling_asset_code=LTC&selling_asset_issuer=GC5LOR3BK6KIOK7GKAUD5EGHQCMFOGHJTC7I3ELB66PTDFXORC2VM5LP&buying_asset_type=native&c=0.37321711147845327"
-        print("Trading Pair is XML<->XRP")
+        print("Trading Pair is XML<->LTC")
     elif TP == "xml_eth":
         url = "https://horizon.stellar.org/order_book?selling_asset_type=credit_alphanum4&selling_asset_code=ETH&selling_asset_issuer=GBDEVU63Y6NTHJQQZIKVTC23NWLQVP3WJ2RI2OTSJTNYOIGICST6DUXR&buying_asset_type=native&c=0.0799001525132561"
         print("Trading Pair is XML<->ETH")
@@ -22,28 +22,28 @@ def extract(TP):
         url = "https://horizon.stellar.org/order_book?selling_asset_type=native&buying_asset_type=credit_alphanum4&buying_asset_code=BTC&buying_asset_issuer=GATEMHCCKCY67ZUCKTROYN24ZYT5GK4EQZ65JJLDHKHRUZI3EUEKMTCH&c=0.9643613563961166"
         print("Trading Pair is XML<->BTC")
     else:
-        print("Currently not support this trading pairs")
+        print("Currently not support this trading pairs",TP)
         return [],[]
     response = requests.get(url)
     content = response.content.decode()
     # Load BIDS List
     bids_list=json.loads(content)['bids']
     #bid_price_list=[]
-    for bid in bids_list:
-        price_r_n =bid['price_r']['n']
-        price_r_d =bid['price_r']['d']
-        price     =bid['price']
-        amount    =bid['amount']
+    #for bid in bids_list:
+    #    price_r_n =bid['price_r']['n']
+    #    price_r_d =bid['price_r']['d']
+    #    price     =bid['price']
+    #    amount    =bid['amount']
         #bid_price_list.append(price)
     # Load ASKS List
     asks_list=json.loads(content)['asks']
     #ask_price_list=[]
-    for ask in asks_list:
-        price_r_n =ask['price_r']['n']
-        price_r_d =ask['price_r']['d']
-        price     =ask['price']
-        amount    =ask['amount']
-        #ask_price_list.append(price)
+    #for ask in asks_list:
+    #    price_r_n =ask['price_r']['n']
+    #    price_r_d =ask['price_r']['d']
+    #    price     =ask['price']
+    #    amount    =ask['amount']
+    #    #ask_price_list.append(price)
 
     #print ('Bids <=============> Asks')
     #for n,g in zip(bid_price_list,ask_price_list):
